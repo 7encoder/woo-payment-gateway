5ca661a6fc39aeae111188738527d7a3b586982b
7encoder <115573019+7encoder@users.noreply.github.com>
2025-10-22 12:32:09 -0400

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '3.2.77.x-dev',
+        'version' => '3.2.77.9999999-dev',
+        'reference' => '6ac2643860d64783fd0d59c4a7368d8f40dd6e6a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '3.2.77.x-dev',
+            'version' => '3.2.77.9999999-dev',
+            'reference' => '6ac2643860d64783fd0d59c4a7368d8f40dd6e6a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'braintree/braintree_php' => array(
+            'pretty_version' => '6.23.0',
+            'version' => '6.23.0.0',
+            'reference' => 'f50b8b95c6cd25bebddc2e411236e823f01f6f3a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../braintree/braintree_php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
